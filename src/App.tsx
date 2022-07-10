1c6037175afd71fe43df8bd37f0c5ce20f558f34
Leandro Portugal <leandroportugal@outlook.com.br>
2022-07-10 20:42:01 -0300

    Initial stylizations

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import * as C from './App.styles';
 const App = () => {
 return(
 <C.Container>
-  ...
+  <C.Header>
+    <C.HeaderText>Controle de Finanças Pessoais</C.HeaderText>
+    </C.Header>
+    <C.Body>
+      ...
+    </C.Body>
 </C.Container>
 
 );
