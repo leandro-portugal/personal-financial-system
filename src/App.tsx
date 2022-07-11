a61ca8d71a58352ba44fdb2c814748069a416639
Leandro Portugal <leandroportugal@outlook.com.br>
2022-07-10 22:42:08 -0300

    working with helpers

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,36 @@
+import { useState, useEffect} from 'react';
 import * as C from './App.styles';
+import { Item } from './types/Item';
+import { Category } from './types/Category';
+import { items } from './data/items';
+import { categories } from './data/categories';
+import { getCurrentMonth } from './helpers/dateFilter'; 
+import { filterListByMonth } from './helpers/dateFilter';
 
 const App = () => {
-return(
-<C.Container>
-  <C.Header>
-    <C.HeaderText>Controle de Finanças Pessoais</C.HeaderText>
-    </C.Header>
-    <C.Body>
-      ...
-    </C.Body>
-</C.Container>
 
-);
+
+
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(()=>{
+    setFilteredList(filterListByMonth(list, currentMonth));
+  }, [list, currentMonth] );
+
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Controle de Finanças Pessoais</C.HeaderText>
+      </C.Header>
+      <C.Body>
+        ...
+      </C.Body>
+    </C.Container>
+
+  );
 
 }
 
